2dabeaa72c4b7d4b75c570ccc6c67b87f1e7d2f2
Avi E Koenig <darkmift@gmail.com>
2024-07-09 21:14:37 +0300

    Add project files.

---
+++ EmployeeCRM/LoginForm.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace EmployeeCRM
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialTextBoxEdit1 = new ReaLTaiizor.Controls.MaterialTextBoxEdit();
+            this.materialLabel1 = new ReaLTaiizor.Controls.MaterialLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.materialLabel2 = new ReaLTaiizor.Controls.MaterialLabel();
+            this.materialTextBoxEdit2 = new ReaLTaiizor.Controls.MaterialTextBoxEdit();
+            this.SuspendLayout();
+            // 
+            // materialTextBoxEdit1
+            // 
+            this.materialTextBoxEdit1.AnimateReadOnly = false;
+            this.materialTextBoxEdit1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.materialTextBoxEdit1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.materialTextBoxEdit1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.materialTextBoxEdit1.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.materialTextBoxEdit1.Depth = 0;
+            this.materialTextBoxEdit1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBoxEdit1.HideSelection = true;
+            this.materialTextBoxEdit1.LeadingIcon = null;
+            this.materialTextBoxEdit1.Location = new System.Drawing.Point(75, 137);
+            this.materialTextBoxEdit1.MaxLength = 32767;
+            this.materialTextBoxEdit1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
+            this.materialTextBoxEdit1.Name = "materialTextBoxEdit1";
+            this.materialTextBoxEdit1.PasswordChar = '\0';
+            this.materialTextBoxEdit1.PrefixSuffixText = null;
+            this.materialTextBoxEdit1.ReadOnly = false;
+            this.materialTextBoxEdit1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.materialTextBoxEdit1.SelectedText = "";
+            this.materialTextBoxEdit1.SelectionLength = 0;
+            this.materialTextBoxEdit1.SelectionStart = 0;
+            this.materialTextBoxEdit1.ShortcutsEnabled = true;
+            this.materialTextBoxEdit1.Size = new System.Drawing.Size(250, 48);
+            this.materialTextBoxEdit1.TabIndex = 0;
+            this.materialTextBoxEdit1.TabStop = false;
+            this.materialTextBoxEdit1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.materialTextBoxEdit1.TrailingIcon = null;
+            this.materialTextBoxEdit1.UseSystemPasswordChar = false;
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto Medium", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.FontType = ReaLTaiizor.Manager.MaterialSkinManager.FontType.H6;
+            this.materialLabel1.Location = new System.Drawing.Point(72, 103);
+            this.materialLabel1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(77, 24);
+            this.materialLabel1.TabIndex = 1;
+            this.materialLabel1.Text = "תעודת זהות";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 335);
+            this.label1.MinimumSize = new System.Drawing.Size(250, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 48);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "כניסה";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 414);
+            this.label2.MinimumSize = new System.Drawing.Size(250, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 48);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "החלפת ססמא";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto Medium", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel2.FontType = ReaLTaiizor.Manager.MaterialSkinManager.FontType.H6;
+            this.materialLabel2.Location = new System.Drawing.Point(72, 210);
+            this.materialLabel2.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(41, 24);
+            this.materialLabel2.TabIndex = 7;
+            this.materialLabel2.Text = "ססמא";
+            // 
+            // materialTextBoxEdit2
+            // 
+            this.materialTextBoxEdit2.AnimateReadOnly = false;
+            this.materialTextBoxEdit2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.materialTextBoxEdit2.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.materialTextBoxEdit2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.materialTextBoxEdit2.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.materialTextBoxEdit2.Depth = 0;
+            this.materialTextBoxEdit2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBoxEdit2.HideSelection = true;
+            this.materialTextBoxEdit2.LeadingIcon = null;
+            this.materialTextBoxEdit2.Location = new System.Drawing.Point(75, 244);
+            this.materialTextBoxEdit2.MaxLength = 32767;
+            this.materialTextBoxEdit2.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
+            this.materialTextBoxEdit2.Name = "materialTextBoxEdit2";
+            this.materialTextBoxEdit2.PasswordChar = '\0';
+            this.materialTextBoxEdit2.PrefixSuffixText = null;
+            this.materialTextBoxEdit2.ReadOnly = false;
+            this.materialTextBoxEdit2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.materialTextBoxEdit2.SelectedText = "";
+            this.materialTextBoxEdit2.SelectionLength = 0;
+            this.materialTextBoxEdit2.SelectionStart = 0;
+            this.materialTextBoxEdit2.ShortcutsEnabled = true;
+            this.materialTextBoxEdit2.Size = new System.Drawing.Size(250, 48);
+            this.materialTextBoxEdit2.TabIndex = 6;
+            this.materialTextBoxEdit2.TabStop = false;
+            this.materialTextBoxEdit2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.materialTextBoxEdit2.TrailingIcon = null;
+            this.materialTextBoxEdit2.UseSystemPasswordChar = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 573);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialTextBoxEdit2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialTextBoxEdit1);
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "כניסה למערכת";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.MaterialTextBoxEdit materialTextBoxEdit1;
+        private ReaLTaiizor.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private ReaLTaiizor.Controls.MaterialLabel materialLabel2;
+        private ReaLTaiizor.Controls.MaterialTextBoxEdit materialTextBoxEdit2;
+    }
+}
